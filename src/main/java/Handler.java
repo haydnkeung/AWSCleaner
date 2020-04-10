63e15424728b392455b4fb78ec93000acc3f9262
haydnkeung <haydnkeung@gmail.com>
2020-04-10 00:31:40 -0400

    Setup Project

---
+++ src/main/java/Handler.java
@@ -0,0 +1,10 @@
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class Handler implements RequestHandler<String, String> {
+
+    @Override
+    public String handleRequest(String input, Context context){
+        return null;
+    }
+}
